4f20cbba03921f61aba069f69f73efabfc8f28ac
KallaGeorg <kalla.georg@yahoo.com>
2023-10-31 18:28:53 +0100

    nästan klart

--- Inlämningsuppgift/HelloUser/src/main/java/com/Prototyp/Prototyp/HelloUserApplication.java
+++ Inlämningsuppgift/HelloUser/src/main/java/com/Prototyp/Prototyp/HelloUserApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PrototypApplication {
+public class HelloUserApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PrototypApplication.class, args);
+		SpringApplication.run(HelloUserApplication.class, args);
 	}
 
 }
